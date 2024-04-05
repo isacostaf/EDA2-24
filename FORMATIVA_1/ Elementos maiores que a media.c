d17d7c0b4ab5c427a2b40c03fe634870213a3ff8
isacostaf <iisabellecostaf@gmail.com>
2024-04-05 11:42:45 -0300

    elementos maiores

---
+++ FORMATIVA_1/ Elementos maiores que a media.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(){
+
+    int entrada;
+
+    scanf("%d", &entrada);
+
+    int v[entrada];
+    int soma;
+    soma = 0;
+
+    for(int i = 0; i < entrada; i++){
+        scanf("%d", &v[i]); // registra o novo elemento ja na pos do vetor
+        soma += v[i]; // soma todos os elmentos adicionado
+    }
+
+    int media;
+    media = soma/entrada;
+    int encontrou = 0;
+
+    for(int i = 0; i < entrada; i++){
+        if(v[i] > media){
+            printf("%d", v[i]);
+            encontrou = 1;
+        }
+    }
+
+    if(!encontrou){
+        printf("0");
+    }
+
+    printf("\n");
+
+    return 0;
+}
